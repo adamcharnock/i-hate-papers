a256c206d02d2fbb092b9a169cf6046e3a734359
Adam Charnock <adam@adamcharnock.com>
2023-08-31 16:50:10 +0200

    Initial work

--- i_hate_papers/openai_utils.py
+++ i_hate_papers/openai_utils.py
@@ -5,11 +5,14 @@ def openai_request(question, text, temperature):
     """Sends a request to a openai large language model."""
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        prompt=f"""{question}:\n\n{text}""",
+        messages=[
+            # {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": f"""{question}:\n\n{text}"""}
+        ],
         temperature=temperature,
         max_tokens=256,
         top_p=1.0,
         frequency_penalty=0.2,
         presence_penalty=0.0,
     )
-    return response["choices"][0]["text"].strip()
+    return response["choices"][0]["message"]["content"].strip()
